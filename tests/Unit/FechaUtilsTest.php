458d472e7f1e87a6964ccefa382e8ade725aaf5d
Tar_Mairon24 <marioirigoyen24@outlook.com>
2025-10-09 11:27:44 -0600

    feat: workflow implementation

--- tests/Unit/FechaUtilsTest.php
+++ tests/Unit/FechaUtilsTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../src/session/FechaUtils.php';
+require_once __DIR__ . '/../../src/session/FechaUtils.php';
 
 class FechaUtilsTest extends TestCase
 {
